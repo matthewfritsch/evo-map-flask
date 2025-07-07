5eeff60a62a0baaafe34dac04460c0b032d9deeb
Matthew Fritsch <matthewmfritsch@gmail.com>
2025-07-07 14:12:12 -0700

    switch to /ant for current loc

--- api/index.py
+++ api/index.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # run_map_update_thread()
 
 
-@app.route("/")
 @app.route("/current")
 def show_map():
     return read_current_map()
@@ -18,6 +17,7 @@ def show_all_map():
     return read_all_map()
 
 
+@app.route("/")
 @app.route("/ant")
 def show_ant_map():
     return read_ant_map()
